4814fab576c1bd4a707c88b929e1a85cc22fba3a
Brenda <brendakelly207@gmail.com>
2025-10-03 02:52:19 +0100

    git commit -m "PWA manifest and sw"
    git add.

--- sw.js
+++ sw.js
@@ -7,25 +7,34 @@ const ASSETS = [
   '/total.html',
   '/style.css',
   '/script.js',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  '/icons.png',
+  '/icons/icon-512.png',
+  
 ];
 
+// Instalação do service worker e cache dos assets
 self.addEventListener('install', (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{})
   );
 });
 
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => res || fetch(evt.request))
-  );
-});
-
+// Ativação e limpeza de caches antigos
 self.addEventListener('activate', (evt) => {
   evt.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
     ))
+  );
+});
+
+// Intercepta requisições e responde do cache, atualizando se possível
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    fetch(evt.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(evt.request, resClone));
+      return res;
+    }).catch(() => caches.match(evt.request))
   );
 });
